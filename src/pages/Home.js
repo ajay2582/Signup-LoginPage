b83f4f6dc1523a9a71e09b3670e833ae7f7da221
ajay2582 <ajaykumarajay2582@gmail.com>
2024-05-04 19:30:06 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full  font-sans'>
+      Welcome To The Study Notion Home Page 
+    </div>
+  )
+}
+
+export default Home
